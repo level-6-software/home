c53382d91a0fcfcc937f1fdb689786ff6d190943
Victor Frunza <victor.frunza96@gmail.com>
2023-04-06 11:18:12 -0600

    admin update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -101,7 +101,7 @@
             </div>
             <div class="mt-16 flow-root sm:mt-24">
                 <div class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                <img src="https://tailwindui.com/img/component-images/project-app-screenshot.png" alt="App screenshot" width="2432" height="1442" class="rounded-md shadow-2xl ring-1 ring-gray-900/10">
+                <img src="/admin-panel.png" alt="App screenshot" width="2432" height="1442" class="rounded-md shadow-2xl ring-1 ring-gray-900/10">
                 </div>
             </div>
             </div>
