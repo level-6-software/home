57976d3ed74dd2f1f3e4e9dc3443984627445ea9
Tyler Rogers <tyler.s.w.rogers@gmail.com>
2023-04-14 14:25:39 -0700

    Update +page.svelte

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,57 +1,86 @@
-<div class="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+<div class="relative isolate bg-white px-6 py-6 sm:py-10 lg:px-8">
     <div class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
       <div class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
-    <div class="mx-auto max-w-2xl text-center lg:max-w-4xl">
+    <div class="mx-auto max-w-2xl text-center lg:max-w-4xl pt-24">
       <h2 class="text-base font-semibold leading-7 text-indigo-600">Pricing</h2>
-      <p class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">The right price for you, whoever you are</p>
+      <p class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">The right price for you, whoever you are.</p>
     </div>
-    <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">Qui iusto aut est earum eos quae. Eligendi est at nam aliquid ad quo reprehenderit in aliquid fugiat dolorum voluptatibus.</p>
-    <div class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-      <div class="rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 bg-white/60 sm:mx-8 lg:mx-0 rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl">
-        <h3 id="tier-hobby" class="text-base font-semibold leading-7 text-indigo-600">Hobby</h3>
+    <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">We charge a single one-time amount based on the scope of the website, and then monthly payments based on your that website's traffic. We don't charge you an extra fee per transaction on top of Stripe, the payment fulfiller.
+    </p>
+    <div class="mx-auto mt-12 max-w-lg items-center grid grid-cols-1 gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-5">
+
+      <div class="lg:col-start-1 lg:col-span-2 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 bg-white/60 sm:mx-8 lg:mx-0">
+        <h3 id="tier-hobby" class="text-base font-semibold leading-7 text-indigo-600">Monthly Cost (Traffic Based)</h3>
         <p class="mt-4 flex items-baseline gap-x-2">
-          <span class="text-5xl font-bold tracking-tight text-gray-900">$19</span>
+          <span class="text-5xl font-bold tracking-tight text-gray-900">$5-15</span>
+          <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">CAD</span>
           <span class="text-base text-gray-500">/month</span>
         </p>
-        <p class="mt-6 text-base leading-7 text-gray-600">The perfect plan if you&#039;re just getting started with our product.</p>
+        <p class="mt-6 text-base leading-7 text-gray-600">Monthly price varies from month to month. Usually around $5/month for low traffic (10-20 visits per month), and around $15/month for higher traffic (~1000 visits per month).
+        </p>
         <ul role="list" class="mt-8 space-y-3 text-sm leading-6 sm:mt-10 text-gray-600">
+
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            25 products
+            Full Dashboard
+          </li>
+
+          <li class="flex gap-x-3">
+            <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+            </svg>
+            Add/Remove products
+          </li>
+          
+          <li class="flex gap-x-3">
+            <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+            </svg>
+            Email notifications on new orders
+          </li>
+
+          <li class="flex gap-x-3">
+            <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+            </svg>
+            Analytics by plausable.io
+          </li>
+
+          <li class="flex gap-x-3">
+            <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+            </svg>
+            Unlimited Visits
           </li>
   
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Up to 10,000 subscribers
+            Technical Support
           </li>
-  
+
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Advanced analytics
-          </li>
-  
-          <li class="flex gap-x-3">
-            <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            24-hour support response time
+            Developers are there to change site content as requested.
           </li>
         </ul>
-        <a href="#" aria-describedby="tier-hobby" class="mt-8 block rounded-md py-2.5 px-3.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10 text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline-indigo-600">Get started today</a>
       </div>
-  
-      <div class="rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 relative bg-gray-900 shadow-2xl">
-        <h3 id="tier-enterprise" class="text-base font-semibold leading-7 text-indigo-400">Enterprise</h3>
+
+      <div class="lg:col-start-3 lg:col-end-4 bg-transparent sm:mx-8 lg:mx-0 flex justify-center">
+        <h2 class="text-3xl font-semibold text-indigo-600">+</h2>
+      </div>
+      
+      <div class="lg:col-start-4 lg:col-span-2 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 relative bg-gray-900 shadow-2xl">
+        <h3 id="tier-enterprise" class="text-base font-semibold leading-7 text-indigo-400">One-Time Cost (Scope Based)</h3>
         <p class="mt-4 flex items-baseline gap-x-2">
-          <span class="text-5xl font-bold tracking-tight text-white">$49</span>
-          <span class="text-base text-gray-400">/month</span>
+          <span class="text-5xl font-bold tracking-tight text-white">$500+</span>
+          <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">CAD</span>
         </p>
         <p class="mt-6 text-base leading-7 text-gray-300">Dedicated support and infrastructure for your company.</p>
         <ul role="list" class="mt-8 space-y-3 text-sm leading-6 sm:mt-10 text-gray-300">
@@ -59,45 +88,45 @@
             <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Unlimited products
+            Perks Here...
           </li>
-  
-          <li class="flex gap-x-3">
-            <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Unlimited subscribers
-          </li>
-  
-          <li class="flex gap-x-3">
-            <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Advanced analytics
-          </li>
-  
-          <li class="flex gap-x-3">
-            <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Dedicated support representative
-          </li>
-  
-          <li class="flex gap-x-3">
-            <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Marketing automations
-          </li>
-  
-          <li class="flex gap-x-3">
-            <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Custom integrations
-          </li>
+
         </ul>
         <a href="#" aria-describedby="tier-enterprise" class="mt-8 block rounded-md py-2.5 px-3.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10 bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500">Get started today</a>
+      </div>
+    </div>
+  </div>
+  
+
+  <div class="bg-white sm:py-4">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl sm:text-center">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">No Tricks.</h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600">We use Stripe to fulfill your payments.</p>
+      </div>
+      <div class="mx-auto mt-4 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+        <div class="py-6 px-12 lg:flex-auto">
+          <h3 class="text-2xl font-bold tracking-tight text-gray-900">Stipe takes a fee per transaction, but we don't</h3>
+          <p class="mt-6 text-base leading-7 text-gray-600">Expect to a fee taken from each purchase made on your site. This is the fee Stripe charges for their payment fulfillment service</p>
+          <p class="mt-6 text-base leading-7 text-gray-600">Expect to a fee taken from each purchase made on your site. This is the fee Stripe charges for their payment fulfillment service</p>
+
+          <div class="m-12 flex justify-center bg-slate-200 rounded-xl font-bold text-sm text-blue-900 py-3 px-4">
+            <a href="https://stripe.com/en-gb-ca/payments">&nbsp; What's Stripe? Learn more about what they do here.</a>
+          </div>
+
+        </div>
+        <div class="lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+          <div class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+            <div class="mx-auto max-w-xs px-8">
+              <p class="mt-6 flex items-baseline justify-center gap-x-2">
+                <span class="text-3xl font-bold tracking-tight text-gray-900">2.9% + $0.30</span>
+                <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">CAD</span>
+              </p>
+              <p class="text-base font-semibold text-gray-600">to stripe per successful card charge</p>
+              <a href="https://stripe.com/en-gb-ca/pricing" class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Learn More</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
